13c160dedc335665cceb629eca97ef8529f98e50
tahasa3ada <tahabenhima@gmail.com>
2024-01-14 19:33:41 +0100

    Delete database/Pstock.sql

--- database/Pstock.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE pstock (
-    codedrouit INTEGER,
-    codefournisseur INTEGER,
-    quantitestock INTEGER,
-    dateajout DATE,
-    PRIMARY KEY (codedrouit, codefournisseur),
-    FOREIGN KEY(codedrouit) REFERENCES produit(code),
-    FOREIGN KEY(codefournisseur) REFERENCES fournisseurs(codef)
-);
